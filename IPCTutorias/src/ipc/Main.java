8e538cc96fc8e2f11bb20596adb453aab47cce40
David@FMR <FMR@Omen-FMR>
2019-11-28 13:15:18 +0100

    Proyecto base parte 2

--- IPCTutorias/src/ipc/Main.java
+++ IPCTutorias/src/ipc/Main.java
@@ -31,7 +31,6 @@ public class Main extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-        System.out.println("Vaya risa Maria Luisa");
     }
     
 }
